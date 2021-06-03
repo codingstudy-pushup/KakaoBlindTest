abd0d6b3c31cdee8ee7e765dedddd1ff9c75a4f9
codingstudy-pushup <52611625+codingstudy-pushup@users.noreply.github.com>
2021-06-03 23:36:23 +0900

    Update A02_MenuRenewal.py

--- 2021/python/A02_MenuRenewal.py
+++ 2021/python/A02_MenuRenewal.py
@@ -1,30 +1,32 @@
 from itertools import combinations
+
+
 def solution(orders, course):
     answer = []
+    dic = dict()
 
-    #orders를 정렬된 리스트로 바꾼다.
-    for i in range(len(orders)):
-        orders[i]=sorted(list(orders[i]))
-        print(orders)
-
-    for i in course:
-        dic=dict()
-        for order in orders:
-            for j in combinations(order,i):
-                temp = ''.join(j)
-                if temp in dic:
-                    dic[temp]+=1
+    for order in orders:
+        for num in course:
+            comb = list(combinations(order, num))
+            for c in comb:
+                c = list(c)
+                c.sort()
+                c = ''.join(c)
+                if c in dic:
+                    dic[c] += 1
                 else:
-                    dic[temp]=1
-        if dic:
-            max_v=dic[max(dic,key=lambda x:dic[x])]
-        for k in dic:
-            if dic[k]==1:
-                continue
-            if dic[k]==max_v:
-                answer.append(k)
-    answer = sorted(answer)
-    return answer
+                    dic[c] = 1
 
-# print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
-print(solution(["XYZ", "XWY", "WXA"],[2,3,4]))
+    for c in course:
+        max_value = 0
+        for k, v in sorted(dic.items(), key=lambda x: x[1], reverse=True):
+            if v >= 2 and len(k) == c:
+                if v >= max_value:
+                    answer.append(k)
+                    max_value = v
+
+    return sorted(answer)
+
+
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4]))
+# print(solution(["AC", "ACE", "ACDE", "CDE"], [2, 3, 4]))
